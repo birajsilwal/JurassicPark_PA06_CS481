dd7317d7d93405c9543c5d775492fde3780b27be
Bethany Pena <penab@unm.edu>
2021-12-05 23:21:05 -0700

    fix compiler errors

--- PA06.c
+++ PA06.c
@@ -3,9 +3,6 @@
 #include <sys/types.h>
 #include <time.h>
 
-int * getHourMinuteSec(int timestep);
-int getMeanArrival(int hour);
-
 int MAXPERCAR;
 int CARNUM;
 int numCarsAvailable;
@@ -20,9 +17,11 @@ pthread_mutex_t mutex;
 struct tm timeInfo ={0};
 
 void getTimeString(int timestep){
-	int hoursMinsSecs [3] = getHourMinuteSec(timestep);
+	int * hoursMinsSecs;
 	char buffer [20];
 	
+	hoursMinsSecs = getHourMinuteSec(timestep);
+
 	timeInfo.tm_sec = hoursMinsSecs[2];
 	timeInfo.tm_min = hoursMinsSecs[1];
 	timeInfo.tm_hour = hoursMinsSecs[0];
@@ -33,82 +32,6 @@ void getTimeString(int timestep){
 	}
 	timeString[8] = '\0';
 }
-
-
-int main(int argc, char *argv[]){
-
-	// Take Input Args	
-	MAXPERCAR = argv[2];
-	CARNUM = argv[4];
-	
-	pthread_t tid;
-	int hours, minutes, seconds;
-	int hoursMinutesSecs[3];
-	int meanArrival;	
-	char timestamp[8];
-	int num_passengers;
-	
-	numCarsAvailable = CARNUM;
-	totalWaitingTime = 0; 
-	totalArrived = 0;
-	totalRejected = 0;
-	totalWaited = 0;
-
-
-	//initialize waiting space
-	for(int i  = 0; i < MAXWAITPEOPLE; i++){
-		waitingSpace[i] = -1;
-	}
-
-
-	for(int timestep = 0; timestep < 600; timestep++){ // 600 minutes = 6000ms == 6 seconds (1 min = 10 ms) -> (1 sec = 0.167 ms)
-
-		num_rejected = 0;
-		num_arrivals = getMeanArrival(hour);
-		num_waiting += num_arrivals;
-		
-		if(num_waiting > MAXWAITPEOPLE){
-			num_rejected = num_waiting - MAXWAITPEOPLE;
-			num_waiting = MAXWAITPEOPLE;
-		}
-		
-		totalRejected += num_rejected;
-		totalArrived += num_arrivals;
-		totalWaited += num_waiting;
-
-		//write to file
-		writeToFile(timestep, num_rejected, num_arrivals, num_waiting);	
-		
-		if(numCarsAvailable != 0){
-			
-			if(num_waiting > MAXPERCAR){
-				num_passengers = MAXPERCAR;
-			}else{
-				num_passengers = num_waiting;
-			}
-			
-			// update waiting time
-			totalWaitingTime += (7 * num_passengers);
-
-			// Send car with thread
-			pthread_create(&tid, NULL, run_car, num_passengers);
-			pthread_join(tid, NULL);
-			
-			//update num_waiting
-			num_waiting -= num_passengers;
-			
-		}
-		
-	}
-
-	printf("Total Number Arrived: %d", totalArrived);
-	printf("Total Number Rejected: %d", totalRejected);
-	printf("Total Number in Line: %d", totalWaited);
-	printf("Average Waiting Time: %d Minutes", (totalWaitingTime / totalWaited) / 60);
-
-    
-}
-
 
 void* run_car(int num_passengers){
 
@@ -122,7 +45,7 @@ void* run_car(int num_passengers){
 		} else {
 			waitingSpace[i] = -1;			
 		}
-		numCarAvailable++;
+		numCarsAvailable++;
 		pthread_mutex_unlock(&mutex);
 		leftPeople--;
 	}
@@ -178,6 +101,86 @@ void writeToFile(int timestep, int num_rejected, int num_arrivals, int num_waiti
 	fclose(output_file);
 
 }
+
+
+
+int main(int argc, char *argv[]){
+
+	// Take Input Args	
+	MAXPERCAR = argv[2];
+	CARNUM = argv[4];
+	
+	pthread_t tid;
+	int hours, minutes, seconds;
+	int hoursMinutesSecs[3];
+	int meanArrival;	
+	char timestamp[8];
+	int num_passengers;
+	int num_arrivals;
+	int num_waiting;
+	int num_rejected;
+	
+	numCarsAvailable = CARNUM;
+	totalWaitingTime = 0; 
+	totalArrived = 0;
+	totalRejected = 0;
+	totalWaited = 0;
+
+
+	//initialize waiting space
+	for(int i  = 0; i < MAXWAITPEOPLE; i++){
+		waitingSpace[i] = -1;
+	}
+
+
+	for(int timestep = 0; timestep < 600; timestep++){ // 600 minutes = 6000ms == 6 seconds (1 min = 10 ms) -> (1 sec = 0.167 ms)
+
+		num_rejected = 0;
+		num_arrivals = getMeanArrival(timestep);
+		num_waiting += num_arrivals;
+		
+		if(num_waiting > MAXWAITPEOPLE){
+			num_rejected = num_waiting - MAXWAITPEOPLE;
+			num_waiting = MAXWAITPEOPLE;
+		}
+		
+		totalRejected += num_rejected;
+		totalArrived += num_arrivals;
+		totalWaited += num_waiting;
+
+		//write to file
+		writeToFile(timestep, num_rejected, num_arrivals, num_waiting);	
+		
+		if(numCarsAvailable != 0){
+			
+			if(num_waiting > MAXPERCAR){
+				num_passengers = MAXPERCAR;
+			}else{
+				num_passengers = num_waiting;
+			}
+			
+			// update waiting time
+			totalWaitingTime += (7 * num_passengers);
+
+			// Send car with thread
+			pthread_create(&tid, NULL, run_car, num_passengers);
+			pthread_join(tid, NULL);
+			
+			//update num_waiting
+			num_waiting -= num_passengers;
+			
+		}
+		
+	}
+
+	printf("Total Number Arrived: %d", totalArrived);
+	printf("Total Number Rejected: %d", totalRejected);
+	printf("Total Number in Line: %d", totalWaited);
+	printf("Average Waiting Time: %d Minutes", (totalWaitingTime / totalWaited) / 60);
+
+    
+}
+
 
 
 
